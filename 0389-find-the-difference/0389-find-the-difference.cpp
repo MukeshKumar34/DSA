caf47bd06e432a6d0d941dabede40bdd86fe2e99
Mukesh Kumar <149522879+MukeshKumar34@users.noreply.github.com>
2026-07-07 21:49:32 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.26%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     char findTheDifference(string s, string t){
-        vector<int>freq(26,0);
+       char final = 0;
 
-        for(char c:s){
-            freq[c-'a']++;
-        }
+       for(char c:s){
+        final ^=c;
+       }
 
-        for(char c:t){
-            freq[c-'a']--;
-        }
+       for(char c:t){
+        final ^=c;
+       }
+return  final;
 
-        for(int i=0;i<26;i++){
-            if(freq[i] == -1){
-                return char(i+'a');
-            }
-        }
-        return ' ';
+
+
     }
 };
