049a37bf86c0e5950dcbe2f7a8e0ed01ccb36955
Mukesh Kumar <149522879+MukeshKumar34@users.noreply.github.com>
2026-09-07 17:16:58 +0530

    Time: 0 ms (100%), Space: 11.6 MB (26.14%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& v) {
         int n = v.size();
-        for(int i = n-1; i >= 0; i--){
-            if(i == n-1)
-                v[i]++;
-            if(v[i] == 10){
-                v[i] = 0;
-                if(i != 0){
-                    v[i-1]++;
-                }
-                else{
-                    v.push_back(0);
-                    v[i] = 1;
-                }
-            }
+       for(int i=n-1;i >=0;i--){
+
+        if(v[i] < 9){
+            v[i]++;
+            return v;
         }
-        return v;
+     v[i] =0;
+       }
+ v.insert(v.begin(),1);
+ return v;
+       
     }
 };
